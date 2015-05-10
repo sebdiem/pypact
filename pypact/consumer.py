75840665116f8ce3ab5aba2169defd19e1c49e06
Rory Hart <hartror@gmail.com>
2015-05-10 21:19:07 +1000

    Added Consumer.has_pact_with.

--- pypact/consumer.py
+++ pypact/consumer.py
@@ -1,7 +1,15 @@
 """Provides API for managing service consumers"""
+from .service import MockService
 
 
 class Consumer(object):
 
-    def __init__(self, name):
+    def __init__(self, name, service_cls=MockService):
         self.name = name
+        self.service_cls = MockService
+
+    def has_pact_with(self, provider, port):
+        return self.service_cls(
+            consumer=self,
+            provider=provider,
+            port=port)
--- pypact/test/test_consumer_cls.py
+++ pypact/test/test_consumer_cls.py
@@ -1,16 +1,28 @@
+import mock
 import pytest
 
-import pypact
+from ..consumer import Consumer
 
 
 CONSUMER_NAME = "My Service Consumer"
 
 
-@pytest.mark.parametrize("args,kwargs", [
-    ([CONSUMER_NAME], {}),
-    ([], {'name': CONSUMER_NAME}),
-])
-def test_consumer_creation(args, kwargs):
-    consumer = pypact.Consumer(*args, **kwargs)
+@pytest.fixture
+def consumer():
+    return Consumer(name=CONSUMER_NAME)
 
+
+def test_consumer_creation(consumer):
     assert consumer.name == CONSUMER_NAME
+
+
+def test_consumer_pact(consumer):
+    service_cls = mock.Mock()
+    consumer.service_cls = service_cls
+    mock_provider = mock.Mock()
+
+    mock_service = consumer.has_pact_with(mock_provider, 1234)
+
+    service_cls.assert_called_once_with(
+        consumer=consumer, provider=mock_provider, port=1234)
+    assert mock_service == service_cls.return_value
