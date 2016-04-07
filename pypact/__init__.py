1ebc18dbb0a822da358bbb88df3dba1e6593f09a
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2016-04-07 21:55:53 +0200

    add Interaction to pypact namespace

--- pypact/__init__.py
+++ pypact/__init__.py
@@ -5,7 +5,8 @@ A consumer driven contract testing library.
 """
 
 from .consumer import Consumer
+from .interaction import Interaction
 from .provider import Provider
 
 
-__all__ = ['Consumer', 'Provider']
+__all__ = ['Consumer', 'Provider', 'Interaction']
